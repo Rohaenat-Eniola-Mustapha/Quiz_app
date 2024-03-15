372b33dd0db208bb73ca61f5102f8805cf7265b0
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-03-15 06:55:49 +0200

    new file, changes made to index, style and script files

---
+++ scripts/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Where is Beijing located?",
+        answer: "C. China",
+        options: [
+            "A. Angola",
+            "B. Spain",
+            "C. China",
+            "D. London"
+        ]
+    },
+    {
+        numb: 2,
+        question: "When did World War II Begin?",
+        answer: "September 1, 1939",
+        options: [
+            "A. November 19, 1900",
+            "B. April 2, 1939",
+            "C. September 1, 2000",
+            "D. September 1, 1939"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Nigeria gained its independence in?",
+        answer: "1960",
+        options: [
+            "A. 1961",
+            "B. 1960",
+            "C. 1959",
+            "D. 1958"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The Genocide began in Rwanda when?",
+        answer: "April 1994",
+        options: [
+            "A. April 1994",
+            "B. April 1995",
+            "C. April 1996",
+            "D. April 1997"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Nelson Mandela became President when?",
+        answer: "1944 to 1999",
+        options: [
+            "A. 1944 to 1998",
+            "B. 1941 to 1999",
+            "C. 1944 to 1999",
+            "D. 1944 to 2000"
+        ]
+    }
+];
